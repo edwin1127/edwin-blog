9e6a13aac2e07ea139fde10ab906c5054dd71110
Edwin Corzo <edwin11ramirez@gmail.com>
2014-10-27 18:44:27 -0700

    i made changes to my blog

--- edwin-blog.php
+++ edwin-blog.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset ="UTF-8"
 	<title></title>
 </head>
 <body>
-<?php  
-
-echo "my name is edwin and im awesome ";
-
-?>
+     <?php  
+       require_once (__DIR__ . "/controller/create-db.php"); 
+     ?>
 
 
 </body>
