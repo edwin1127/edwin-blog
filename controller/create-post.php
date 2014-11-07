906e96ce0c370c5a9bd5ce4f8d023f021973ffb2
Edwin Corzo <edwin11ramirez@gmail.com>
2014-11-07 11:00:56 -0800

    added more files

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,2 +1,10 @@
 <?php 
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $post  = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+    //this is a post where we send in an information
+    //this post is unique to php
+    //this is so we can filter the input
+    echo "<p>Title: $title</p>";
+    echo "<p>Title: $post</p>";
+
 	//this folder is to create a post and push it to our data base 
--- view/header.php
+++ view/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset ="UTF-8"
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+    <meta charset ="UTF-8">
 	<title></title>
 	 <!--were going to be reusing the same files here-->
 </head>
