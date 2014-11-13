28e1dd40bc151624ccb1cb65de9a6cedab96f48f
Edwin Corzo <edwin11ramirez@gmail.com>
2014-11-13 11:48:29 -0800

    created a blog post and inserted to php my admin

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,11 +1,32 @@
 <?php 
+    require_once(__DIR__ . "/../model/database.php");
+    //we use .. to get out of the folder
+
+    $connection = new mysqli($host, $username, $password, $database);
+    //were connecting all the files together
+
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post  = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     //this is a post where we send in an information
     //this post is unique to php
     //this is so we can filter the input
-    echo "<p>Title: $title</p>";
-    echo "<p>Title: $post</p>";
-    
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post='$post'");
+    //were using single quoytes to make sure it sends
+    //we use insert into posts to set the title & post
+    //were inserting the posts
+    //setting certain values
+
+    if ($query) {
+    	echo "<p>Successfully interested post : $title</p>";
+    } else {
+    	echo "<p>$connection->errror</p>";
+    }
+    //were using this to make it either true or false and make it succesfull
+    //if its true your succesfull
+    //if its false we have a problem
+
+
+    $connection->close();
+
 
 	//this folder is to create a post and push it to our data base 
--- view/form.php
+++ view/form.php
@@ -1,16 +1,25 @@
+<?php
+	require_once(__DIR__ ."/../model/config.php");
+	//were repeating th steps that we did before
+	//we use the .. to get out of the view
+?>
+
 <h1>CREATE BLOG POST </h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
+
+<!--the form is to send information and recieve information-->
+<!-- this is to help reach certain files-->
     <div>
         <label for="title"> Title: </label>
-        <input type="text" />
+        <input type="text" name="title" />
     <!--the input is a box where you can type in the box-->
 	<!--this div is mainly going to be for title-->
     </div>
 
     <div>
         <label for="post">Post: </label>
-        <textarea name="postS"></textarea>
+        <textarea name="post"></textarea>
     <!-- this is going to be mainly for post-->
     </div>
 
